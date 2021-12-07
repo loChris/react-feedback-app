bc952723a94caed480da0edbdbc9a8e579762054
Chris <44450518+loChris@users.noreply.github.com>
2021-12-06 22:02:49 -0400

    added dynamic values and lists

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 function App() {
-  return <h1>Hello from the app component</h1>;
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ];
+
+  return (
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      <div className="comments">
+        <h3>Comments</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
