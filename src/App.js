15b3ca844f6c3dca1ac4a8c9cef474a6a88ba446
Chris <44450518+loChris@users.noreply.github.com>
2022-01-02 17:50:51 -0400

    removed the closing tag for one of the Routes

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
                 />
               </>
             }
-          ></Route>
+          />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
         <AboutIconLink />
